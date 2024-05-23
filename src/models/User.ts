6e958ce4849280c67e3fb308a1537433a3c4909d
adkshishir <adhikarishishir50@gmail.com>
2024-05-24 00:59:20 +0545

    add : get profile

--- src/models/User.ts
+++ src/models/User.ts
@@ -16,5 +16,44 @@ class User {
     });
     return user;
   }
+  async getProfile(id: string) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: id,
+        },
+        select: {
+          name: true,
+          email: true,
+          carts: {
+            select: {
+              id: true,
+              quantity: true,
+              product: {
+                select: {
+                  name: true,
+                  slug: true,
+                },
+              },
+            },
+          },
+          Wishlists: {
+            select: {
+              id: true,
+              product: {
+                select: {
+                  name: true,
+                  slug: true,
+                },
+              },
+            },
+          },
+        },
+      });
+      return user;
+    } catch (error: any) {
+      throw error.message;
+    }
+  }
 }
 export default new User();
