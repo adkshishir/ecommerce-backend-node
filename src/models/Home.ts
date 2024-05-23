a8727890de2fa2b846e000cb418e345ab3501c2f
adkshishir <adhikarishishir50@gmail.com>
2024-05-24 01:02:39 +0545

    update : search query

--- src/models/Home.ts
+++ src/models/Home.ts
@@ -1,4 +1,5 @@
 import { PrismaClient, Product } from '@prisma/client';
+import { searchQueryType } from '../controllers/productController';
 const prisma = new PrismaClient();
 
 class Home {
@@ -177,28 +178,112 @@ class Home {
       .slice(0, 4);
     return topSellingProducts;
   }
-  async searchProduct(query: string) {
-    const products = prisma.product.findMany({
-      where: {
-        name: {
-          contains: query,
+  async searchProduct(searchQuery: searchQueryType) {
+    let varientSomeCondition:
+      | {
+          some: {
+            OR: {
+              value: string;
+            }[];
+          };
+        }
+      | undefined = undefined;
+    if (!searchQuery.color && !searchQuery.size) {
+      varientSomeCondition = undefined;
+    } else if (!searchQuery.color && searchQuery.size) {
+      varientSomeCondition = {
+        some: {
+          OR: [{ value: searchQuery.size }],
         },
-      },
-      select: {
-        name: true,
-        slug: true,
-        media: {
-          select: {
-            url: true,
-          },
-          take: 1,
-          orderBy: {
-            id: 'desc',
-          },
+      };
+    }
+    if (!searchQuery.size && searchQuery.color) {
+      varientSomeCondition = {
+        some: {
+          OR: [{ value: searchQuery.color }],
         },
-      },
-    });
-    return products;
+      };
+    }
+    if (searchQuery.color && searchQuery.size) {
+      varientSomeCondition = {
+        some: {
+          OR: [
+            {
+              value: searchQuery.color,
+            },
+            {
+              value: searchQuery.size,
+            },
+          ],
+        },
+      };
+    }
+    console.log(searchQuery);
+    let skip: number = 0;
+    let take: number = 10;
+
+    if (Number(searchQuery.currentPage) && Number(searchQuery.pageSize)) {
+      skip =
+        (Number(searchQuery.currentPage) - 1) * Number(searchQuery.pageSize);
+      take = Number(searchQuery.pageSize);
+    } else if (Number(searchQuery.currentPage)) {
+      skip = (Number(searchQuery.currentPage) - 1) * 10;
+    } else if (Number(searchQuery.pageSize)) {
+      take = Number(searchQuery.pageSize);
+    }
+
+    try {
+      const products = await prisma.product.findMany({
+        skip,
+        take,
+        where: {
+          name: {
+            contains: searchQuery.name,
+          },
+          markedPrice: {
+            gte: Number(searchQuery.minPrice) || 0,
+            lte: Number(searchQuery.maxPrice) || 1000000,
+          },
+          variants: varientSomeCondition,
+        },
+        select: {
+          media: {
+            select: {
+              url: true,
+              alt: true,
+            },
+          },
+          category: {
+            select: {
+              name: true,
+              slug: true,
+              parent: {
+                select: {
+                  name: true,
+                  slug: true,
+                },
+              },
+            },
+          },
+          variants: {
+            select: {
+              value: true,
+              type: true,
+            },
+          },
+          name: true,
+          slug: true,
+          markedPrice: true,
+          discount: true,
+          id: true,
+        },
+      });
+      const totalProducts = await prisma.product.count();
+      return { products, totalProducts };
+    } catch (error: any) {
+      // throw error.message;
+      return false;
+    }
   }
   async getSpecialCategories() {
     const specialCategories = prisma.category.findMany({
