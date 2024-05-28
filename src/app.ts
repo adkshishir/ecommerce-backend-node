0c7a8e2feb9b6e16645164d1171c1b7019510e7a
adkshishir <adhikarishishir50@gmail.com>
2024-05-28 06:59:09 +0545

    fix : cors error

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', '*'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use('*', cors(corsOptions));
