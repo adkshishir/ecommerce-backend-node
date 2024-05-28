365102c52f8f9c89a7ea7918ace03350cb72f250
adkshishir <adhikarishishir50@gmail.com>
2024-05-28 22:08:04 +0545

    fix :  undefined userId on updating wishlist

--- src/models/WishList.ts
+++ src/models/WishList.ts
@@ -3,14 +3,31 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 class Wishlist {
     async store(data: any) {
-        try {
-            const wishlist = await prisma.wishlist.create({
-                data: data,
-            });
-            return wishlist;
-        } catch (error) {
-            throw error;
+      try {
+        // check if the product is liked or not already
+        const check = await prisma.wishlist.findFirst({
+          where: {
+            userId: data.userId,
+            productId: data.productId,
+          },
+        });
+        if (check) {
+          // unliked the product
+          const wishlist = await prisma.wishlist.delete({
+            where: {
+              id: check.id,
+            },
+          });
+          return wishlist;
+        } else {
+          const wishlist = await prisma.wishlist.create({
+            data: data,
+          });
+          return wishlist;
         }
+      } catch (error) {
+        throw error;
+      }
     }
 
     async update(id: number, data: any) {
@@ -56,9 +73,32 @@ class Wishlist {
     async getByUserId(id: string) {
         try {
             const wishlist = await prisma.wishlist.findMany({
-                where: {
-                    userId: id,
+              where: {
+                userId: id,
+              },
+              select: {
+                id: true,
+                productId: true,
+                product: {
+                  select: {
+                        name: true,
+                        totalStocks: true,
+                        markedPrice: true,
+                        discount: true,
+                    slug: true,
+                    media: {
+                      select: {
+                        url: true,
+                        alt: true,
+                      },
+                      take: 1,
+                      orderBy: {
+                        id: 'desc',
+                      },
+                    },
+                  },
                 },
+              },
             });
             return wishlist;
         } catch (error) {
